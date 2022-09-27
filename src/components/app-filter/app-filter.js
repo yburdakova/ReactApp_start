ad95ad757865cf26dc1a71e6d2a55b4a1903f75e
Yana Burdakova <bu.asti@gmail.com>
2022-09-26 23:36:17 -0400

    ReactApp_start

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,19 @@
+import "./app-filter.css"
+
+const AppFilter = () => {
+    return(
+        <div className="btn-group">
+            <button 
+            className="btn btn-light"
+            type ="button">All emploees</button>
+            <button 
+            className="btn btn-outline-light"
+            type ="button">For bonuses</button>
+            <button 
+            className="btn btn-outline-light"
+            type ="button">Salary over $1000</button>
+        </div>
+    );
+}
+
+export default AppFilter;
---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting for Kinderlogia employees</h1>
+            <h2>Number of employees: </h2>
+            <h2>Number of award nominees: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
